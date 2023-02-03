935818190f8499d09ebb1c55625c1f1fbbfaeeab
NickFrol1 <nickfrol85@gmail.com>
2023-02-03 19:26:10 +0300

    123

--- src/test/java/ru/geekbrains/my/first/at/example1/NavigationTest.java
+++ src/test/java/ru/geekbrains/my/first/at/example1/NavigationTest.java
@@ -2,28 +2,51 @@ package ru.geekbrains.my.first.at.example1;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
+import com.fasterxml.jackson.core.util.BufferRecycler;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.openqa.selenium.By;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 @Execution(ExecutionMode.CONCURRENT)
 public class NavigationTest {
 
     @Test
     public void checkNavigation(){
-        String tempString;
+        String tmpLine;
+        String resLine ="172.18.0.2";
+        String port = "80";
+        Process proc = null;
         try {
-            tempString = Runtime.getRuntime().exec("docker ps").getOutputStream().toString();
+            proc = Runtime.getRuntime().exec("docker inspect 4a | grep IPAddress");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        System.out.println(tempString);
+        InputStream in = proc.getInputStream();
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        try {
+            while ((tmpLine = br.readLine()) != null) {
+                if (tmpLine.contains("\"IPAddress\": \"172")) {
+                    resLine = tmpLine.replace("\"IPAddress\": \"","").
+                            replaceAll(" ", "").
+                            replace("\"","").
+                            replace(",", "");
+                    System.out.println(resLine);
+                }
+            };
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
 //        Selenide.open(" https://localhost:8088/index.html");
-        Selenide.open("https://drom.ru");
-        Selenide.$(By.linkText("Продажа авто в России")).click();
+        Selenide.open("https://"+resLine+port+":/");
+//        Selenide.$(By.linkText("Продажа авто в России")).click();
 
 //        assert Selenide.$(By.cssSelector(".css-hqbmxg")).getText().contains("Продажа автомобилей");
 //        Selenide.$("[class=\"gb-left-menu__nav\"] [href=\"/courses\"]").click();
